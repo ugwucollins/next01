8cfedaffc7138eb8cb84c6ecf2dbbdd576a7a4b5
ugwucollins <ugwucollins027@gmail.com>
2024-05-10 14:24:56 +0100

    my First Next App
    done

--- nextapp/src/app/page.tsx
+++ nextapp/src/app/page.tsx
@@ -7,7 +7,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
        <div>
-          <h1>Hello <span className="text-2xl">{Inti}</span> First Next App</h1>
+          <h1>Hello <span className="text-2xl">{Inti}</span>
+           First Next App</h1>
+           <br />
           <Link href='/users' className="text-base hover:text-blue-500 transition-all">
              Users Page </Link>
        </div>
